2e48ce65b03f8fa93063f3e55f00a154cce618a4
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-08-11 15:21:14 +0545

    event listener in java

---
+++ event1.java
@@ -0,0 +1,31 @@
+
+// event delegation model
+// event source- where event occurs
+// event object- contains info abt event ActionEvent,MouseEvent
+// event listener- respond to the event by exec. the code
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class event1 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Event listner");
+        JButton btn = new JButton("click me");
+
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("button is clicked");
+            }
+        });
+
+        frame.add(btn);
+        frame.setSize(150, 100);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+
+}
